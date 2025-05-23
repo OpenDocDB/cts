ca6f4818f765dfeed94eb91ae9025edea24378cc
noisersup <patryk@kwiatek.xyz>
2025-05-23 14:03:55 +0200

    wip

---
+++ opendocdb-cts/test_result_test.go
@@ -0,0 +1,51 @@
+// Copyright 2025 Peter Farkas, Alexey Palazhchenko
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import "testing"
+
+func TestResultsTable(t *testing.T) {
+	tests := map[string]struct {
+		results  []testResult
+		expected string
+	}{
+		"empty": {
+			results: []testResult{},
+			expected: "" +
+				"Test Name     |Result\n" +
+				"---------     |------\n",
+		},
+		"simple": {
+			results: []testResult{
+				{name: "test1", passed: true},
+				{name: "test2", passed: false},
+			},
+			expected: "" +
+				"Test Name     |Result\n" +
+				"---------     |------\n" +
+				"test1         |✅\n" +
+				"test2         |❌\n",
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			result := resultsTable(tc.results)
+			if result != tc.expected {
+				t.Errorf("expected %q, got %q", tc.expected, result)
+			}
+		})
+	}
+}
