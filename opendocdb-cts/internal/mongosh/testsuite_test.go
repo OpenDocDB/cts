6b5016c91e5e0229c998fa47dbf9cd7ff600eb22
noisersup <patryk@kwiatek.xyz>
2025-05-27 18:11:25 +0200

    wip

--- opendocdb-cts/internal/mongosh/testsuite_test.go
+++ opendocdb-cts/internal/mongosh/testsuite_test.go
@@ -84,3 +84,39 @@ func TestConvertResponse(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, unindent(expected)+"\n", actual)
 }
+
+func TestConvertResponseLongString(t *testing.T) {
+	longText := "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+		"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in " +
+		"reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, " +
+		"sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+	res := wirebson.MustDocument(
+		"cursor", wirebson.MustDocument(
+			"firstBatch", wirebson.MustArray(
+				wirebson.MustDocument("_id", "long-string", "v", longText),
+				wirebson.MustDocument("_id", "int64-zero", "v", int64(0)),
+			),
+			"id", int64(0),
+			"ns", "test.values",
+		),
+	)
+
+	expected := `
+	response = {
+	"cursor": {
+	"firstBatch": [
+	{
+	"_id": "long-string",
+	"v": Lorem
+	},
+	],
+	"id": Long(0),
+	"ns": "test.values"
+	}
+	}`
+
+	actual, err := ConvertResponse(res)
+	require.NoError(t, err)
+	assert.Equal(t, unindent(expected)+"\n", actual)
+}
