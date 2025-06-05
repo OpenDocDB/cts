2e45005dbc7dd9b32cd664a25a9e8958ad9faca3
noisersup <patryk@kwiatek.xyz>
2025-06-05 15:59:06 +0200

    wip

---
+++ cts-mongosh/aggregation/stages/bucket/simple.request.js
@@ -0,0 +1,29 @@
+db.runCommand({
+"aggregate": "values",
+"pipeline": [
+{
+"$bucket": {
+"groupBy": "$v",
+"boundaries": [
+0,
+50,
+100
+],
+"default": "Other",
+"output": {
+"count": {
+"$sum": 1
+}
+}
+}
+},
+{
+"$sort": {
+"_id": 1
+}
+}
+],
+"cursor": {
+
+}
+});
---
+++ cts-mongosh/aggregation/stages/bucket/simple.response.js
@@ -0,0 +1,17 @@
+response = {
+"cursor": {
+"firstBatch": [
+{
+"_id": 0,
+"count": 14
+},
+{
+"_id": "Other",
+"count": 34
+}
+],
+"id": Long(0),
+"ns": "db.values"
+},
+"ok": Double(1)
+}
