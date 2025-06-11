f4870e99e40436b957904f82738baab1d3ed62cc
noisersup <patryk@kwiatek.xyz>
2025-06-11 14:49:45 +0200

    wip

--- opendocdb-cts/internal/testresult/test_result.go
+++ opendocdb-cts/internal/testresult/test_result.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// testresult provides functionality to format test suite results into a table.
 package testresult
 
 import (
